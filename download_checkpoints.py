fcf2259ce9d3b824f3e8490743a3829fa23c6d45
Diego Braga <diebraga0055@gmail.com>
2026-08-31 17:01:26 +0100

    Add script to download model checkpoints and weights
    The script creates necessary directories and downloads model checkpoints and weights from specified URLs.

---
+++ download_checkpoints.py
@@ -0,0 +1,23 @@
+import urllib.request
+import os
+
+os.makedirs('/app/SadTalker/checkpoints', exist_ok=True)
+os.makedirs('/app/SadTalker/gfpgan/weights', exist_ok=True)
+
+files = [
+    ('https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00109-model.pth.tar', '/app/SadTalker/checkpoints/mapping_00109-model.pth.tar'),
+    ('https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00229-model.pth.tar', '/app/SadTalker/checkpoints/mapping_00229-model.pth.tar'),
+    ('https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/SadTalker_V0.0.2_256.safetensors', '/app/SadTalker/checkpoints/SadTalker_V0.0.2_256.safetensors'),
+    ('https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/SadTalker_V0.0.2_512.safetensors', '/app/SadTalker/checkpoints/SadTalker_V0.0.2_512.safetensors'),
+    ('https://github.com/xinntao/facexlib/releases/download/v0.1.0/alignment_WFLW_4HG.pth', '/app/SadTalker/gfpgan/weights/alignment_WFLW_4HG.pth'),
+    ('https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth', '/app/SadTalker/gfpgan/weights/detection_Resnet50_Final.pth'),
+    ('https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth', '/app/SadTalker/gfpgan/weights/GFPGANv1.4.pth'),
+    ('https://github.com/xinntao/facexlib/releases/download/v0.2.2/parsing_parsenet.pth', '/app/SadTalker/gfpgan/weights/parsing_parsenet.pth'),
+]
+
+for url, path in files:
+    print(f'Downloading {url}')
+    urllib.request.urlretrieve(url, path)
+    print(f'Saved {path}')
+
+print('All checkpoints downloaded.')
